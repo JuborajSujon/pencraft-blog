fd6bd8b059cfe3823da316dc13e44f44fdf45e4e
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-29 22:30:40 +0600

    create Authentication error function

---
+++ src/app/errors/AuthenticationError.ts
@@ -0,0 +1,23 @@
+class AuthenticationError extends Error {
+  public statusCode: number;
+  public details: string;
+
+  constructor(
+    statusCode: number,
+    details: string,
+    message: string,
+    stack = '',
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.details = details;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export default AuthenticationError;
