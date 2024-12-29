14de5072055e13a326e7b516759f03baad5c0a64
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-29 20:05:41 +0600

    create user model

---
+++ src/app/module/user/user.model.ts
@@ -0,0 +1,38 @@
+import { model, Schema } from 'mongoose';
+import { IUser } from './user.interface';
+
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        'Please provide a valid email address',
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const UserModel = model<IUser>('User', userSchema);
